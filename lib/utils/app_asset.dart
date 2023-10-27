8d190385e1cb562965857ffb4355c3aac69974a0
AmrAbdelsattar <amrmmohamed00@gmail.com>
2023-10-28 01:00:41 +0200

    Initial commit

---
+++ lib/utils/app_asset.dart
@@ -0,0 +1,10 @@
+abstract class AppAsset{
+  final String ball = "assets/images/ball.png";
+  final String business = "assets/images/business.png";
+  final String environment = "assets/images/environment.png";
+  final String health = "assets/images/health.png";
+  final String newsTest = "assets/images/NewsTest.png";
+  final String politics = "assets/images/Politics.png";
+  final String science = "assets/images/science.png";
+  final String splash = "assets/images/splash.png";
+}
---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColor {
+  final Color primary = const Color.fromARGB(255, 57, 164, 82);
+  final Color white = Colors.white;
+  final Color grey = const Color.fromARGB(255, 78, 89, 105);
+  final Color sportsBackGround = const Color.fromARGB(255, 199, 28, 35);
+  final Color politicsBackGround = const Color.fromARGB(255, 0, 62, 144);
+  final Color healthBackGround = const Color.fromARGB(255, 235, 30, 121);
+  final Color businessBackGround = const Color.fromARGB(255, 205, 125, 72);
+  final Color environmentBackGround = const Color.fromARGB(255, 71, 129, 206);
+  final Color scienceBackGround = const Color.fromARGB(255, 240, 209, 82);
+}
